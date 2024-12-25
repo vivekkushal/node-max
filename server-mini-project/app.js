28ae55f3b8126043e45778cf62acd3750eb108cf
vivekkushal <vivekjss1@gmail.com>
2024-12-25 18:38:13 +0530

    feat: Basic node.js server complete

--- server-mini-project/app.js
+++ server-mini-project/app.js
@@ -1,17 +1,49 @@
 const http = require('node:http');
+const fs = require('fs');
 
-// runs on every incoming request -> event driven architecture of NodeJS
-function reqListener(req, res) {
-  console.log(req.url, req.method, req.headers);
-  // process.exit(1);
+// the inside function runs on every incoming request -> event driven architecture of NodeJS
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    // .on() is an event listener on the request object
+    // the 'data' event will be fired whenever a new chunk is ready to be read
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    // aliter
+    // res.writeHead('302', {
+    //   Location: '/',
+    // });
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
-}
+});
 
-const server = http.createServer(reqListener);
-
-server.listen('4000');
+server.listen('3000');
