ee3f0b0aa2aa4d3b787d0451257026ffa92c1b13
vivekkushal <vivekjss1@gmail.com>
2024-12-25 08:59:25 +0530

    feat: Add headers, write html

--- server-mini-project/app.js
+++ server-mini-project/app.js
@@ -2,7 +2,14 @@ const http = require('node:http');
 
 // runs on every incoming request -> event driven architecture of NodeJS
 function reqListener(req, res) {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit(1);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 }
 
 const server = http.createServer(reqListener);
