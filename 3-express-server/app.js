86a1918451f394d9dcf9752823ff1caa88e920aa
vivekkushal <vivekjss1@gmail.com>
2025-01-04 14:14:46 +0530

    feat: Add body parsing

--- 3-express-server/app.js
+++ 3-express-server/app.js
@@ -2,18 +2,27 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next(); // Allows the request to continue to the next middleware in line
-});
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', (req, res, next) => {
+//   console.log('This always runs!');
+//   next(); // Allows the request to continue to the next middleware in line
+// });
 
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The Add Product page</h1>');
+  // console.log('In another middleware!');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
+  // console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
